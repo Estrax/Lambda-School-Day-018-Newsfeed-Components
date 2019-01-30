dda7fd24f602141d417be7348beab556d67c20e2
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-01-30 17:38:43 -0500

    added gsap and jquery animations

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    this.expandButton.textContent = "Click to expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
     
@@ -17,7 +17,16 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    
     this.domElement.classList.toggle("article-open");
+    
+    if(this.domElement.classList.contains('article-open')){
+      TweenMax.to(this.domElement, .5, {height: 400});
+      this.expandButton.textContent = 'Click to Close';
+    }else{
+      TweenMax.to(this.domElement, .5, {height: 50});
+      this.expandButton.textContent = 'Click to Expand';
+    };
   }
 
   deleteArticle() {
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,4 +1,3 @@
-
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle("menu--open");
@@ -9,4 +8,6 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', () => toggleMenu());
+
+// changed to jquery slide
+// menuButton.addEventListener('click', () => toggleMenu());
